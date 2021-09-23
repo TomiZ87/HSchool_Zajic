1535b347ec72cd4ca2f89165ecdb21d51901bac0
TomiZ Dev <zajic.tomas@4hobbygarden.com>
2021-09-23 13:21:39 +0200

    Added assignment1_input.py

---
+++ Assignment 1 JuniorY/assignment1_input.py
@@ -0,0 +1,40 @@
+name = input("Zadaj svoje meno: ")
+print("Tomáš")
+
+name = input("Zadaj svoje meno: ")
+print("Volám sa "+ name)
+
+name = input("Zadaj svoje meno: ")
+last_name = input("Zadaj svoje priezvisko: ")
+print(name + " a " + last_name)
+
+a = input("Zadaj ľubovoľné slovo: ")
+print(a)
+a = input("Zadaj druhé ľubovoľné slovo: ")
+print(a)
+
+a = "Today is 23 Sept. 2021"
+b = a
+print(a)
+print(b)
+b = "Today is Thursday"
+print(a)
+print(b)
+
+a = "testing"
+b = "Tomáš"
+c = a
+a = b
+b = c
+print(a + b)
+
+dog = "cat"
+cat = "dog"
+print(dog)
+print(cat)
+
+word1 = "desu."
+word2 = "Tomu "
+word3 = "wa "
+word4 = "Watashi "
+print(word4 + word3 + word2 + word1)
