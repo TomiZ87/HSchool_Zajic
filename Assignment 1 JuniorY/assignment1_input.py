4e168bbe6a14f7cb5f925f55b62ebe2dcbefced5
TomiZ Dev <tomiz87dev@gmail.com>
2021-09-30 12:50:19 +0200

    Details in Assignment 1 JuniorY A1

--- Assignment 1 JuniorY/assignment1_input.py
+++ Assignment 1 JuniorY/assignment1_input.py
@@ -18,31 +18,31 @@ a = input("Enter 2nd any word: ")
 print(a)
 
 print("Exercise 6: Putting a variable to another variable")
-a = input("Enter any sentence:")
+a = input("Enter any sentence: ")
 b = a
 print(a)
 print(b)
-b = input("Enter 2nd any sentence:")
+b = input("Enter 2nd any sentence: ")
 print(a)
 print(b)
 
 print("Exercise 7: Swaping and Joining Sentences")
-a = input("Enter any sentence:")
-b = input("Enter 2nd any sentence:")
+a = input("Enter any sentence: ")
+b = input("Enter 2nd any sentence: ")
 c = a
 a = b
 b = c
 print(a + b)
 
 print("Exercise 8: Enter the given words")
-dog = input("Enter the word cat")
-cat = input("Enter the word dog")
+dog = input("Enter the word cat: ")
+cat = input("Enter the word dog: ")
 print(dog)
 print(cat)
 
 print("Exercise 9: Creating 4 word sentence from the back")
-word1 = input("Enter 4th word")
-word2 = input("Enter 3rd word")
-word3 = input("Enter 2nd word")
-word4 = input("Enter 1st word")
+word1 = input("Enter 4th word: ")
+word2 = input("Enter 3rd word: ")
+word3 = input("Enter 2nd word: ")
+word4 = input("Enter 1st word: ")
 print(word4 + " " + word3 + " " + word2 + " " + word1)
