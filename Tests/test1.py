bc0716ef232166159ae0e8d38d96679ec288d7f2
TomiZ Dev <tomiz87dev@gmail.com>
2021-11-17 19:30:14 +0100

    Test1 Update

--- Tests/test1.py
+++ Tests/test1.py
@@ -15,10 +15,8 @@ first_name_error = ""
 last_name_error = ""
 job_error = ""
 
+age=1
 # 1.1 Age and Date Logic
-error_c_month = 0
-error_b_month = 0
-same_month = 0
 
 valid_current_month = 1
 valid_birth_month = 1
@@ -27,7 +25,7 @@ valid_current_day = 1
 valid_birth_day = 1
 valid_days = 1
 
-# 1.1.1 Checking if the entered months are valid
+# 1.1.2 Checking if the entered months are valid
 if current_month >= 1 and current_month <= 12:
     valid_current_month = 1
 else:
@@ -97,26 +95,30 @@ else:
     valid_birth_day = 0
 if bool(valid_current_day) and bool(valid_birth_day):
     valid_days = 1
+
 # 1.1.3 Checking the age
-
-if bool(valid_months) and bool(valid_days) and current_month > birth_month:
+if bool(valid_days) and bool(valid_months) and current_month > birth_month:
     age = current_year - birth_year
-elif bool(valid_months) and bool(valid_days) and current_month < birth_month:
+elif bool(valid_days) and bool(valid_months) and current_month < birth_month:
     age = current_year - birth_year - 1
-elif bool(valid_months) and bool(valid_days) and current_month == birth_month and current_day >= birth_day:
+elif bool(valid_days) and bool(valid_months) and current_month == birth_month and current_day >= birth_day:
     age = current_year - birth_year
+if bool(valid_days) and bool(valid_months) and age < 0:
+    age_error = "The user hasn't been born yet" 
+if not bool(valid_birth_day) or not bool(valid_current_day):
+    age_error = "Entered birth or current day don't exist."
+if not bool(valid_birth_month) or not bool(valid_current_month):
+    age_error = "Entered birth or current month don't exist."
 
-if bool(valid_months) and bool(valid_days) and age < 0:
-    age_error = "Error: The User Was not born yet!"
-
+# 1.1.4 Dates
 if bool(valid_current_month) and bool(valid_current_day):
     current_date = str(current_day) + "." + str(current_month) + "." + str(current_year)
 else:
-    current_date = "Unknown"
+    current_date = "Unknown -> (" + str(current_day) + "." + str(current_month) + "." + str(current_year) + ")"
 if bool(valid_birth_month) and bool(valid_birth_day):
     birth_date = str(birth_day) + "." + str(birth_month) + "." + str(birth_year)
 else:
-    current_date = "Unknown"
+    birth_date = "Unknown -> (" + str(birth_day) + "." + str(birth_month) + "." + str(birth_year) + ")"
 
 # 1.2 Names logic
 first_name_count = 1
@@ -125,31 +127,27 @@ if " " in first_name:
 if first_name == "":
     first_name_error = "Error: No first name was entered!"
     first_name = "Unknown"
+    first_name_count = 0
 if last_name == "":
     last_name_error = "Error: No last name was entered!"
     last_name = "Unknown"
 
 # 1.3 Job logic
-if age < 18 and not age == "Unknown":
-    job = "Student"
-if age < 18 and not age == "Unknown" and not job == "Student" and not job == "student":
+if bool(valid_days) and bool(valid_months) and 0 < age < 18 and (job != "Student" or not job != "student"):
     job = "Unknown"
     job_error = "Error: The Student has got a job!!"
+elif bool(valid_days) and bool(valid_months) and 0 < age < 18:
+    job = "Student"
 if job == "":
     job_error = "Error: No job was entered"
     job = "Unknown"
 
-# 1.1 Age errors for 
-if not bool(valid_months):
+# 1.1.5 Age Error unknowns
+if not age_error == "":
     age = str(age)
     age = "Unknown"
-    age_error = "Error: The entered month/s are not valid!"
-if not bool(valid_months):
-    age = str(age)
-    age = "Unknown"
-    age_error = "Error: The entered month/s are not valid!"
 
-# 1.4 Printing Data
+# 1.4.1 Printing Data
 print("User Data:")
 print("First Name: " + str(first_name))
 print("First Name Count: " + str(first_name_count))
@@ -158,11 +156,13 @@ print("Current Date: " + str(current_date))
 print("Birth Date: " + str(birth_date))
 print("Age: " + str(age))
 print("Job: " + str(job))
-if first_name == "Unknown":
+
+# 1.4.2 Printing Errors
+if first_name_error != "":
     print("First Name Error: " + first_name_error)
-if last_name == "Unknown":
+if last_name_error != "":
     print("Last Name Error: " + last_name_error)
-if age == "Unknown":
+if age_error != "":
     print("Age Error: " + age_error)
-if job == "Unknown":
+if job_error != "":
     print("Job Error: " + job_error)
