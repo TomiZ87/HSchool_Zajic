7f99b6819b8d48ec6b33182821bef159dda60ca4
TomiZ Dev <tomiz87dev@gmail.com>
2022-02-03 11:51:34 +0100

    Number Guessing Game

---
+++ Tests/random_number.py
@@ -0,0 +1,29 @@
+import random
+print("Number Guessing Game:");
+attempts = int(input("Enter the number of attempt for this game: "));
+max_number = int(input("Enter the max number you want to guess (have to be more than 0): "));
+cur_attempt = 1;
+guessing_num = random.randint(0, max_number);
+numbers = []
+
+if max_number > 0:
+    while cur_attempt <= attempts:
+        if cur_attempt == attempts:
+            print("Warning!! Last Attempt no. " +str(cur_attempt));
+        else:
+            print("Attempt : " + str(cur_attempt));
+        guess = int(input("Guess Number: "));
+        if guess != guessing_num:
+            numbers.append(guess);
+            if guessing_num > guess:
+                print("The number is higher!")
+            else:
+                print("The number is lower!")
+        if guess == guessing_num:
+            print("You won within the entered attempts, Guessed Number is indeed " + str(guessing_num));
+            break;
+        if cur_attempt == attempts:
+            print("You lose, you used all your " + str(attempts) + " attempts. The correct answer was " + str(guessing_num));
+            print("You entered these numbers " + str(numbers));
+            break;
+        cur_attempt += 1;
