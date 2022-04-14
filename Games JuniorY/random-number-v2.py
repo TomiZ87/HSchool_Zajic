1faf5df279fbc1dee7e69577ac73d962fa3ba8c4
TomiZ Dev <tomiz87dev@gmail.com>
2022-04-14 23:44:18 +0200

    Random Number Game v2

--- Games JuniorY/random-number-v2.py
+++ Games JuniorY/random-number-v2.py
@@ -26,14 +26,14 @@ while num2 < num1:
 print("The range is " +str(num1) + "-" + str(num2))
 attempts = int((num2-num1)/2)
 guessing_num = random.randint(num1, num2)
-print(guessing_num)
 round = 1
 
 win = False
+winwin = False
 while round < attempts and not win:
     print("\n\nRound " +str(round))
     for x in range(0, players_num):
-        if not win:
+        if not winwin:
             if players[x]["mode"] == 1:
                 players[x]["numbers"].append(int(input(str(players[x]["nickname"])+ " Guess: ")))
             else:
@@ -44,24 +44,28 @@ while round < attempts and not win:
                 print("The number is lower!")
                 if num2 > players[x]["numbers"][round-1]:
                     num2 = players[x]["numbers"][round-1]
-                    print("The range is " +str(num1) + "-" + str(num2))
-                    round += 1
             elif players[x]["numbers"][round-1] < guessing_num:
                 print("The number is higher!")
                 if num1 < players[x]["numbers"][round-1]:
                     num1 = players[x]["numbers"][round-1]
-                    print("The range is " +str(num1) + "-" + str(num2))
-                    round += 1
             print(str(attempts) + " attempts left!")
             if players[x]["numbers"][round-1] == guessing_num:
                 print("\n\nWow, " +str(players[x]["nickname"])+ " You are right, the correct number is " +str(guessing_num))
-                win = True
+                winwin = True
+        else:
+            if players[x]["mode"] == 1:
+                players[x]["numbers"].append(str("n/a"))
+            else:
+                players[x]["numbers"].append((str("n/a")))
+    if winwin:
+        win = True
+    round += 1
     
 
 if attempts == 0:
     print("\n\nYou all lost")
 print("The correct answer was " +str(guessing_num))
 print("Here is your history:")
-for x in range(1, round+1):
-    for y in players:
+for x in range(1, round):
+    for y in range(0, players_num):
         print("Round " +str(x) + " " +players[y]["nickname"]+ ": " +str(players[y]["numbers"][x-1]))
