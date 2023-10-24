49117e541c71961f227c49cfe5af5cdff2935315
TomiZ Dev <tomiz87dev@gmail.com>
2023-10-24 14:49:12 +0200

    Assignment - Number Guessing (Hádanie čísel) v2

--- Super Senior Maturita Year/18102023-NumGuess v4.py
+++ Super Senior Maturita Year/18102023-NumGuess v4.py
@@ -1,17 +1,15 @@
 import random
 import math
 range1, range2 = 0, 100
-x = range2 + 1
+x = round((range2 - range1) / 2)
 guess = random.randrange(range1, range2+1)
 while x != guess:
-    if x == (range2 + 1): x = 50
-    else:
-        if guess > x:
-            range1 = x
-            print("The number is higher. Current attempt:", x)
-            x = x + math.ceil((range2 - x) / 2)
-        elif guess < x:
-            range2 = x
-            print("The number is lower. Current attempt:", x)
-            x = x - math.ceil((x - range1) / 2)
-    if x == guess: print("Indeed! The number was:", x)
+    if guess > x:
+        range1 = x
+        print("The number is higher. Current attempt:", x)
+        x = x + math.ceil((range2 - x) / 2)
+    elif guess < x:
+        range2 = x
+        print("The number is lower. Current attempt:", x)
+        x = x - math.ceil((x - range1) / 2)
+if x == guess: print("Indeed! The number was:", x)
