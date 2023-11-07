bae77e4ec2c5e40868f99c734de2b4fb3f2c42bf
TomiZ Dev <tomiz87dev@gmail.com>
2023-11-07 15:19:59 +0100

    Assignment  - Preklad čísel do binárnej sústavy

--- Super Senior Maturita Year/02102023-Decimal to Binary.py
+++ Super Senior Maturita Year/02102023-Decimal to Binary.py
@@ -1,13 +1,13 @@
 import math
-def get_number():
+def get_number(prompt):
     while True:
         try:
-            number = int(input("Enter the number in base 10: "))
+            number = int(input(prompt))
             if number > -1: return number
             else: print("The number is lower than 0!")
         except ValueError: print("Invalid input. Please write")
-def main():
-    number = get_number()
+def DecimalToBinary():
+    number = get_number("Enter the number in base 10: ")
     number = float(number)
     final = ""
     while True:
@@ -19,4 +19,19 @@ def main():
             if number == 1: break
             number = math.floor(number / 2)
     print("Number in binary: " + final)
+def BinaryToDecimal():
+    binary = get_number("Enter the number in binary: ")
+    binary_array = list(str(binary)[::-1])
+    binary_array = list(map(int, binary_array))
+    final = 0
+    for x in range(len(binary_array)):
+        final = ((2**x)*binary_array[x]) + final
+    print("Number in binary: " + str(final)) 
+def main():
+    while True:
+        choice = input("Enter 1: DecimalToBinary, 2: BinaryToDecimal, 3 to quit the app: ")
+        if choice == '1': DecimalToBinary()
+        elif choice == '2': BinaryToDecimal()
+        elif choice == '3': break
+        else: print("\033[31mInvalid choice. Please enter 1, 2, or 3.\033[0m")
 if __name__ == "__main__": main()
