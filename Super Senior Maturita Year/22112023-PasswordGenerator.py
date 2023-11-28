798e63d0f66fefca6b46b2efbd12aacb6c989579
TomiZ Dev <tomiz87dev@gmail.com>
2023-11-28 15:02:14 +0100

    Assignment - Generátor hesla (update)

--- Super Senior Maturita Year/22112023-PasswordGenerator.py
+++ Super Senior Maturita Year/22112023-PasswordGenerator.py
@@ -2,25 +2,15 @@ import random
 special_characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-def get_integer(promt):
-    while True:
-        try:
-            number = int(input(promt))
-            if number > 0: return number
-            else: print("\033[31mPlease enter a positive number.\033[0m")
-        except ValueError: print("\033[31mInvalid input. Please enter a positive number.\033[0m")
-lenght = get_integer("Enter the lenght of your password: ")
+password_key = input("Enter the structure of the password C(Digit), M(small letters), V(capital letters, S(special characters)): ")
+password_key = password_key.upper()
+for i in password_key:
+    if i == "C" or i == "S" or i == "M" or i == "V": pass
+    else: exit()
 password = []
-
-for x in range(0, lenght):
-    y = random.randint(0, 3)
-    if y == 0:
-        password.append(random.choice(special_characters))
-    elif y == 1:
-        password.append(random.choice(numbers))
-    elif  y == 2:
-        password.append(random.choice(letters))
-    elif  y == 3:
-        upper_letter = random.choice(letters)
-        password.append(upper_letter.lower())
+for x in password_key:
+    if x == "S": password.append(random.choice(special_characters))
+    elif x == "C": password.append(random.choice(numbers))
+    elif  x == "V": password.append(random.choice(letters))
+    elif  x == "M": password.append(random.choice(letters).lower())
 print("".join(password))
