b2bb1763f4a0ea497f742ac29a412f59a18ec4ad
TomiZ Dev <tomiz87dev@gmail.com>
2023-11-29 13:50:37 +0100

    Assignment - Primes (v2)

--- Super Senior Maturita Year/22112023-Primes.py
+++ Super Senior Maturita Year/22112023-Primes.py
@@ -1,23 +1,19 @@
-def get_integer(promt):
+def get_float(promt):
     while True:
         try:
             number = float(input(promt))
-            return number
+            if number % 1 == 0: return number 
+            else: print("The number is decimal, try again!")
         except ValueError: print("\033[31mInvalid input. Please enter a number.\033[0m")
-true = True
-while true:
-    number = get_integer("Please enter the number: ")
-    if number % 1 == 0:
-        number = int(number)
-        if number != 0:
-            for i in range(2, abs(number)):
-                if number % i == 0:
-                    print("The number is not prime!")
-                    break
-                elif (number % i != 0 and i == (abs(number)-1)):
-                    print("The number is prime!")
+number = get_float("Please enter the number: ")
+number = int(number)
+if number != 0:
+    for i in range(2, abs(number)):
+        if abs(number) % i == 0:
+            print("The number is not prime!")
             break
-        else:
-            print("The number is 0, try again!")
-    else:
-        print("The number is decimal, try again!")
+        elif (abs(number) % i != 0 and i == abs(number)-1):
+            print("The number is prime!")
+    if number == 2: print("The number is prime!")
+else:
+    print("The number is 0, try again!")
