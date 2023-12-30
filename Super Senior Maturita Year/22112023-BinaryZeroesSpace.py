de6d7ae4925951c0c7f0ece4f686152cc7b93c29
TomiZ Dev <tomiz87dev@gmail.com>
2023-12-30 10:31:22 +0100

    Assignment - Binary Zero spaces (update)

--- Super Senior Maturita Year/22112023-BinaryZeroesSpace.py
+++ Super Senior Maturita Year/22112023-BinaryZeroesSpace.py
@@ -1,21 +1,19 @@
-while True:
-    binary_zeroes = str(input("Please enter the binary: "))
-    binary = "01"
-    for x in binary_zeroes:
-        if x not in binary:
-            print(x)
-            print("The number isnt binary")
-            exit()
-    zeroes = 0
-    zeroes_array = []
-    binary_zeroes = int(binary_zeroes)
-    binary_zeroes = str(binary_zeroes)
-    for y in binary_zeroes:
-        if y == "1":
-            zeroes_array.append(zeroes)
-            zeroes = 0
-        if y == "0":
-            zeroes += 1
-    break
+binary_zeroes = str(input("Please enter the binary: "))
+binary = "01"
+for x in binary_zeroes:
+    if x not in binary:
+        print(x)
+        print("The number isnt binary")
+        exit()
+zeroes = 0
+zeroes_array = []
+binary_zeroes = int(binary_zeroes)
+binary_zeroes = str(binary_zeroes)
+for y in binary_zeroes:
+    if y == "1":
+        zeroes_array.append(zeroes)
+        zeroes = 0
+    if y == "0":
+        zeroes += 1
 print("The number has " +str(max(zeroes_array)) + " binary 0 spaces.")
     
