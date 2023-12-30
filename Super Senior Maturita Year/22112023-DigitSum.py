b8b3659681e97cf667505f1857ba5a2cb42d9249
TomiZ Dev <tomiz87dev@gmail.com>
2023-12-30 10:33:36 +0100

    Assignment - Ciferný súčet (update)

--- Super Senior Maturita Year/22112023-DigitSum.py
+++ Super Senior Maturita Year/22112023-DigitSum.py
@@ -2,7 +2,7 @@ def get_integer(promt):
     while True:
         try:
             number = int(input(promt))
-            if number > 0: return number
+            if number >= 0: return number
             else: print("\033[31mPlease enter a positive number.\033[0m")
         except ValueError: print("\033[31mInvalid input. Please enter a positive number.\033[0m")
 number1 = get_integer("Enter a whole number <: ")
