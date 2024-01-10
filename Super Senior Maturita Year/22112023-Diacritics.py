c15e1644377dfa8d8f6bbcc6d8815c98eaaa1aa6
TomiZ Dev <tomiz87dev@gmail.com>
2024-01-10 23:36:12 +0100

    Assignment - Diacritics (update)

--- Super Senior Maturita Year/22112023-Diacritics.py
+++ Super Senior Maturita Year/22112023-Diacritics.py
@@ -1,11 +1,10 @@
+import string
 text = input("Enter the text: ")
-diacritics = ["á", "Á", "é", "É", "ě", "Ě", "ä", "Ä", "ó", "Ó", "ú", "Ú", "ý", "Ý", "ĺ", "Ĺ", "ŕ", "Ŕ", "č", "Č", "ď", "Ď", "ľ", "Ľ", "ň", "Ň", "š", "Š", "ť", "Ť", "ž", "Ž", "ô", "Ô", "ł", "Ł"]
+diacritics = string.ascii_letters
 diacritics_count = 0
 for x in text:
-    if x in diacritics:
+    if x not in diacritics:
         print("Diacritics found " + x)
         diacritics_count += 1
-if diacritics_count == 0:
-    print("No diacritics found!")
-else:
-    print(str(diacritics_count) + " diacritics found.")
+if diacritics_count == 0: print("No diacritics found!")
+else: print(str(diacritics_count) + " diacritics found.")
