94c0946656fd0cbbc8202853e40c704e8c924efe
TomiZ Dev <tomiz87dev@gmail.com>
2024-04-07 22:49:11 +0200

    Assignment - Primes 2 or sth like that

--- Super Senior Maturita Year/26032024-Primes2.py
+++ Super Senior Maturita Year/26032024-Primes2.py
@@ -1,3 +1,4 @@
+import time
 def get_integer(promt):
     while True:
         try:
@@ -6,13 +7,17 @@ def get_integer(promt):
             else: print("The number is decimal, try again!")
         except ValueError: print("\033[31mInvalid input. Please enter a number.\033[0m")
 number = get_integer("Please enter the number: ")
+start_time = time.time()
 if abs(number) > 2 and (abs(number) % 2 != 0):
     x = 3
     while (abs(number) > x):
         if abs(number) % x == 0:
-            print("The number is not prime!")
-            exit()
+            print("The number is not prime! Last prime " + str(x))
+            break
         x += 1
     else: print("The number is prime!")
-elif abs(number) < 3: print("The number is prime!")
-else: print("The number is not prime!")
+elif abs(number) < 3 and abs(number) > 1: print("The number is prime!")
+elif abs(number) == 1: print("Idk, just 1")
+else: print("The number is not prime! Last prime 2")
+end_time = time.time()
+print("Execution time:", (end_time - start_time), "seconds")
